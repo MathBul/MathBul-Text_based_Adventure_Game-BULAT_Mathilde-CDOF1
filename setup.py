6bd124a5992f9304ed2830db2435907756101ac5
MathBul <145438871+MathBul@users.noreply.github.com>
2024-01-22 14:13:01 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+name='{Mathbul-Test_based_Adventure_Game-BULAT_Mathilde-CDOF1}',
+version='1.0',
+author='{MathBul}',
+license='MIT',
+long_description=open('README.md').read(),
+)
